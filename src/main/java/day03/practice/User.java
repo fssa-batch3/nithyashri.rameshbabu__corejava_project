27e2331d728e05340d36d2fb9b7027039a07ac98
Nithi-fssa <118426030+Nithi-fssa@users.noreply.github.com>
2023-07-12 11:02:43 +0530

    Initial Commit

---
+++ src/main/java/day03/practice/User.java
@@ -0,0 +1,59 @@
+package day03.practice;
+
+public class User {
+      
+	private String name;
+	private String email;
+	private String pass;
+	
+	
+	public User() {	
+	}
+	
+	public User(String name , String email, String pass) {
+		this.name = name;
+		this.email = email;
+		this.pass = pass;
+				
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void SetName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void SetEmail(String email) {
+		this.email = email;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void SetPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	public static void main(String[]args) {
+		User one = new User();
+		one.SetName("Nithyashri");
+		one.SetEmail("nithirmsh@gmail.com");
+		one.SetPass("A0109");
+		
+		System.out.println("User 1");
+		System.out.println("Name" + " = " + one.getName());
+		System.out.println("Email" + " = " + one.getEmail());
+		System.out.println("Password" + " = " +  one.getPass());
+		
+		System.out.println();
+		User two = new User("Ramesh" , "ramesh@gmail.com", "A0908");
+		System.out.println("User 2");
+		System.out.println("Name" + " = " + two.getName());
+		System.out.println("Email" + " = " + two.getEmail());
+		System.out.println("Password" + " = " + two.getPass());
+		
+	}
+}
